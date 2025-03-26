bb4dca32b81ce3d74cca17e981b783dd1513105a
Andrea Ang <andreaangxx@gmail.com>
2025-03-26 00:01:05 -0400

    Updated Changes

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,43 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+        const cost = parseFloat(item.cost.substring(1)); 
+        const quantity = item.quantity;
+        total += cost * quantity;
+    });
+    return total;  
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(item.name, item.quantity + 1));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1)
+        dispatch(updateQuantity(item.name, item.quantity - 1));
+    else if (item.quantity == 1)
+        dispatch(removeItem(item.name));
   };
 
   const handleRemove = (item) => {
+        dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.substring(1));
+    const quantity = item.quantity;
+    return cost * quantity;
   };
 
   return (
